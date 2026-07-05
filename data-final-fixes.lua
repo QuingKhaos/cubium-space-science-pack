4c52dd523f25b210bf554f216cab4ca76df65156
Em Karisch <emii.khaos@gmail.com>
2026-07-05 17:25:22 +0200

    feat: add support for modifications from muluna

--- data-final-fixes.lua
+++
@@ -1,6 +0,0 @@
--- Re-copy crafting time, in case it got changed by another mod
-local khaoslib_recipe = require("__khaoslib__.recipe")
-
-khaoslib_recipe:load("space-science-pack-cubic")
-  :set {energy_required = data.raw["recipe"]["space-science-pack"].energy_required}
-  :commit()
